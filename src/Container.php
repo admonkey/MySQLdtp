78f44047404f792bf34c0300a50e3ea3cef1753c
Jeff Puckett <jeff@jeffpuckett.com>
2016-10-10 22:22:39 -0500

    add registry to Container

--- src/Container.php
+++ src/Container.php
@@ -1,5 +1,6 @@
 <?php
 namespace jpuck\dbdtp;
+use Exception;
 
 // http://stackoverflow.com/q/3715518/4233593
 class Container {
@@ -10,5 +11,17 @@ class Container {
 			return $this->objects[$class];
 		}
 		return $this->objects[$class] = new $class();
+	}
+
+	protected static $registry = [];
+	public static function bind($key, $value){
+		static::$registry[$key] = $value;
+	}
+	public static function get($key){
+		if(!array_key_exists($key, static::$registry)){
+// 			throw new Exception("No $key is bound in the container.");
+			return null;
+		}
+		return static::$registry[$key];
 	}
 }
