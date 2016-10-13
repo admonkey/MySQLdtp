06d2f23a1e2a2031e9ebc49f5d5308dc654ac79f
Jeff Puckett <jeff@jeffpuckett.com>
2016-10-12 22:20:14 -0500

    set short alias if namespaced

--- src/App.php
+++ src/App.php
@@ -13,7 +13,15 @@ class App {
 			if(!class_exists($key)){
 				return null;
 			}
+
+			// dynamically create instance
 			static::$registry[$key] = new $key;
+
+			// set short alias if namespaced
+			// http://stackoverflow.com/a/27457689/4233593
+			if($name = substr(strrchr($key, '\\'), 1)){
+				static::$registry[$name] =& static::$registry[$key];
+			}
 		}
 		return static::$registry[$key];
 	}
