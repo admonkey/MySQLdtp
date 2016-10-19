f6f987915528a08536fb4ffd89cc0223741c91d4
Jeff Puckett <jeff@jeffpuckett.com>
2016-10-18 21:37:29 -0500

    add command execute sql

---
+++ src/Execute.php
@@ -0,0 +1,32 @@
+<?php
+namespace jpuck\dbdtp;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Execute extends Command {
+	protected function configure(){
+		$this->setName('execute')
+			->setDescription('Execute SQL scripts')
+			->addArgument(
+				'pdo',
+				InputArgument::OPTIONAL,
+				'File that returns an instance of PDO'
+			)->addArgument(
+				'sql',
+				InputArgument::OPTIONAL,
+				'SQL script file'
+			);
+	}
+
+	public function execute(InputInterface $input, OutputInterface $output){
+		App::bind('io', new IO($this, $input, $output))
+			->title('Execute');
+
+		$pdo = require App::get('in')->getArgument('pdo');
+		$pdo->exec(file_get_contents(App::get('in')->getArgument('sql')));
+	}
+}
